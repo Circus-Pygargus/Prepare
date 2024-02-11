a2f633d6905b17bacf2d0010e89a78ac78e6cfc0
Circus-Pygargus <richard.meuret@laposte.net>
2024-02-11 16:34:20 +0100

    oups : forgot to commit webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('user-role-form-autofill', './assets/js/form/change-user-role-form-autofill.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
