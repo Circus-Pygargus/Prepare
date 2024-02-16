167bbb1898391fb15a90cd2f410b6e962e615cb4
Circus-Pygargus <richard.meuret@laposte.net>
2024-02-17 00:07:21 +0100

    Migration first admin : change admin role by super admin and rename  file

--- migrations/Version20240216230000_insert_admin.php
+++ migrations/Version20240216230000_insert_admin.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240212175500_insert_admin extends AbstractMigration
+final class Version20240216230000_insert_admin extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20240212175500_insert_admin extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('INSERT INTO user (username, password, roles, is_active, created_at)
-              VALUES ("Riri", "$2y$13$AZbTJLOKQRwT5DQLGHJOleoK6aMcl0FYO/LwG.0CqMJyBBUKCgNz.", \'["ROLE_ADMIN"]\', true, NOW())');
+              VALUES ("Riri", "$2y$13$AZbTJLOKQRwT5DQLGHJOleoK6aMcl0FYO/LwG.0CqMJyBBUKCgNz.", \'["ROLE_SUPER_ADMIN"]\', true, NOW())');
 
     }
 
