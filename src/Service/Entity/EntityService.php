e49ed6c1fcf3e6a6afe339d7c496b8aeabff0b89
Circus-Pygargus <richard.meuret@laposte.net>
2024-04-16 18:29:41 +0200

    Remove old slug services

--- src/Service/Entity/EntityService.php
+++ src/Service/Entity/EntityService.php
@@ -2,24 +2,19 @@
 
 namespace App\Service\Entity;
 
-use App\Service\String\SluggerService;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class EntityService
 {
     public function __construct(
         private Security $security,
-        private SluggerService $slugger,
     ) {
     }
 
     /**
      * Handles properties which are not in forms
      *
-     * @param object $updatedObject
-     * @param object $originalObject
-     * @param array $params, needed keys :
-     *      'slugSource' => the property used to build slug
+     * @param object $object
      * @return object
      */
     public function handleCommonProperties(object $object): object
